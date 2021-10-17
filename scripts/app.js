6797209eb0e7afdad3443425deedcbaa7f24a8b9
jwachtel96 <jwachtel96@gmail.com>
2021-10-17 19:41:36 -0400

    10/17

--- scripts/app.js
+++ scripts/app.js
@@ -7,8 +7,58 @@ const app = Sammy("#main", function () {
 	});
 
 	this.get("#/recipes", function (context) {
+		const recipes = [
+			{
+				id: 1,
+				name: "Spaghetti",
+				imageURL: "../images/pasta.png",
+				ingredients: [
+					"test1",
+					"test2",
+					"test3",
+					"test4",
+					"test5",
+					"test6",
+				],
+				details:
+					"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+			},
+			{
+				id: 2,
+				name: "Spaghetti",
+				imageURL: "../images/pasta.png",
+				ingredients: [
+					"test1",
+					"test2",
+					"test3",
+					"test4",
+					"test5",
+					"test6",
+				],
+				details:
+					"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+			},
+			{
+				id: 3,
+				name: "Spaghetti",
+				imageURL: "../images/pasta.png",
+				ingredients: [
+					"test1",
+					"test2",
+					"test3",
+					"test4",
+					"test5",
+					"test6",
+				],
+				details:
+					"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+			},
+		];
+		context.recipes = recipes;
+
 		context.partial("../views/recipes.hbs");
 	});
+
 	this.get("#/login", function (context) {
 		context.partial("../views/login.hbs");
 	});
@@ -16,11 +66,9 @@ const app = Sammy("#main", function () {
 	this.get("#/register", function (context) {
 		context.partial("../views/registration.hbs");
 	});
-	// this.get("#/login", userController.getLogin);
 
 	// this.post("#/register", userController.postRegister);
 	// this.post("#/login", userController.postLogin);
-	// this.get("#/logout", userController.logout);
 });
 
 (() => {
