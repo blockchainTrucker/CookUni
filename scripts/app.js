fc837e6c28bce4ca400813a246f10030e99c354c
jwachtel96 <jwachtel96@gmail.com>
2021-10-20 19:47:35 -0400

    10/20/21

--- scripts/app.js
+++ scripts/app.js
@@ -3,72 +3,182 @@ const app = Sammy("#main", function () {
 
 	// Home
 	this.get("#/home", function (context) {
-		context.partial("../views/home.hbs");
+		context
+			.loadPartials({
+				header: "../views/header.hbs",
+				footer: "../views/footer.hbs",
+			})
+			.then(function () {
+				this.partial("../views/home.hbs");
+			});
 	});
 
 	this.get("#/recipes", function (context) {
 		const recipes = [
 			{
-				id: 1,
-				name: "Spaghetti",
+				id: "-MmFhxK7MGD9dlghOtzK",
+				description: "lots of words",
 				imageURL: "../images/pasta.png",
-				ingredients: [
-					"test1",
-					"test2",
-					"test3",
-					"test4",
-					"test5",
-					"test6",
-				],
-				details:
-					"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-			},
-			{
-				id: 2,
+				ingredients: ["test1", "test2", "test3", "test4"],
 				name: "Spaghetti",
-				imageURL: "../images/pasta.png",
-				ingredients: [
-					"test1",
-					"test2",
-					"test3",
-					"test4",
-					"test5",
-					"test6",
-				],
-				details:
-					"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-			},
-			{
-				id: 3,
-				name: "Spaghetti",
-				imageURL: "../images/pasta.png",
-				ingredients: [
-					"test1",
-					"test2",
-					"test3",
-					"test4",
-					"test5",
-					"test6",
-				],
-				details:
-					"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
 			},
 		];
 		context.recipes = recipes;
 
-		context.partial("../views/recipes.hbs");
+		context
+			.loadPartials({
+				header: "../views/header.hbs",
+				footer: "../views/footer.hbs",
+			})
+			.then(function () {
+				this.partial("../views/recipes.hbs");
+			});
+	});
+
+	this.get("#/details/:id", function (context) {
+		const recipe = {
+			id: "-MmFhxK7MGD9dlghOtzK",
+			description: "lots of words",
+			imageURL: "../images/pasta.png",
+			ingredients: ["test1", "test2", "test3", "test4"],
+			name: "Spaghetti",
+		};
+		context.name = recipe.name;
+		context.description = recipe.description;
+		context.imageURL = recipe.imageURL;
+		context.ingredients = recipe.ingredients;
+		context
+			.loadPartials({
+				header: "../views/header.hbs",
+				footer: "../views/footer.hbs",
+			})
+			.then(function () {
+				this.partial("../views/details.hbs");
+			});
+	});
+
+	this.get("#/profile", function (context) {
+		const recipes = [
+			{
+				id: "-MmFhxK7MGD9dlghOtzK",
+				description: "lots of words",
+				imageURL: "../images/pasta.png",
+				ingredients: ["test1", "test2", "test3", "test4"],
+				name: "Spaghetti",
+			},
+		];
+
+		context.recipes = recipes;
+
+		const user = {
+			firstName: "John",
+			lastName: "Doe",
+			email: "john@example.com",
+		};
+
+		context.firstName = user.firstName;
+		context.lastName = user.lastName;
+		context.email = user.email;
+
+		context
+			.loadPartials({
+				header: "../views/header.hbs",
+				footer: "../views/footer.hbs",
+			})
+			.then(function () {
+				this.partial("../views/profile.hbs");
+			});
 	});
 
 	this.get("#/login", function (context) {
-		context.partial("../views/login.hbs");
+		context
+			.loadPartials({
+				header: "../views/header.hbs",
+				footer: "../views/footer.hbs",
+			})
+			.then(function () {
+				this.partial("../views/login.hbs");
+			});
 	});
 	// User
 	this.get("#/register", function (context) {
-		context.partial("../views/registration.hbs");
+		context
+			.loadPartials({
+				header: "../views/header.hbs",
+				footer: "../views/footer.hbs",
+			})
+			.then(function () {
+				this.partial("../views/registration.hbs");
+			});
 	});
 
-	// this.post("#/register", userController.postRegister);
-	// this.post("#/login", userController.postLogin);
+	this.post("#/register", function () {
+		// console.log(this.params);
+		let data = {
+			firstName: this.params.regFirst,
+			lastName: this.params.regLast,
+			username: this.params.regUser,
+			password: this.params.regPass,
+		};
+		let url = "https://cookuni96-default-rtdb.firebaseio.com/users.json";
+		let headers = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		};
+		fetch(url, headers)
+			.then(function (response) {
+				if (response.status == 200) {
+					window.location.hash = "#/profile";
+				} else {
+					console.log(response.status);
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	});
+
+	this.get("#/create", function (context) {
+		context
+			.loadPartials({
+				header: "../views/header.hbs",
+				footer: "../views/footer.hbs",
+			})
+			.then(function () {
+				this.partial("../views/create.hbs");
+			});
+	});
+
+	this.post("#/create", function (context) {
+		let data = {
+			name: this.params.name,
+			imageURL: this.params.imageURL,
+			description: this.params.description,
+			ingredients: this.params.ingredients,
+		};
+		let url = "https://cookuni96-default-rtdb.firebaseio.com/recipes.json";
+		let headers = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		};
+		fetch(url, headers)
+			.then(function (response) {
+				if (response.status == 200) {
+					window.location.hash = "#/profile";
+				} else {
+					console.log(response.status);
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	});
 });
 
 (() => {
