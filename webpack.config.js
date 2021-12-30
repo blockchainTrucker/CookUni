c8c5f97bdfedfd9fb66e4ffe9a89466f1a839865
Jesse Wachtel <jwachtel96@gmail.com>
2021-12-30 14:54:25 -0500

    12/30

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
 	mode: "production",
-	entry: path.resolve(__dirname, "scripts/app.js"),
+	entry: path.resolve(__dirname + "/scripts/", "app.js"),
 	output: {
 		filename: "bundle.js",
 		path: path.resolve(__dirname, "dist"),
